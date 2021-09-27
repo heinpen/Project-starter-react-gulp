a3e174c3136b7470b71532e7fc80d40829adf654
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-09-27 18:48:18 +0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const config = {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }]
+  }
+};
+
+module.exports = config
